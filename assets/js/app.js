332f3f17df8d34c33fe8acd19a043c0bb48e2aa0
Akshay Rajput <ra901625072@gmail.com>
2026-08-04 17:17:01 +0530

    style: increase theme toggle transition duration to 1s for a smoother sweeping ripple effect

--- assets/js/app.js
+++ assets/js/app.js
@@ -267,7 +267,7 @@ function initTheme() {
           clipPath: clipPath
         },
         {
-          duration: 650,
+          duration: 1000,
           easing: "cubic-bezier(0.16, 1, 0.3, 1)", // Fluid Apple-like decel curve
           pseudoElement: "::view-transition-new(root)"
         }
